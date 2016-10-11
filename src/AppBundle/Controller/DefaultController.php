97c58990ea5ab47a3002f0253304ddde964e63c1
usuario <usuario@MacBook-Pro-de-usuario.local>
2016-10-11 16:35:46 +0200

    Se eliminan lineas comentadas innecesarias

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,31 +4,19 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 
 class DefaultController extends Controller
 {
-    private $local = false; // false por defecto
-    private $id_app = 0;
-    private $id_op  = 0;
-
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
         $response = new Response();
         $response
             ->setContent(file_get_contents(__DIR__.'/../Resources/views/Default/index.html'));
-
-//        $auth_ok = $this->get('check.authorization')
-//            ->checkAuth($request, $this->id_app, $this->id_op);
-//
-//        if($auth_ok)
-            return $response;
-//        else
-//            return $this->get('slogin.authz')->login();
+        return $response;
     }
 }
